4fdfe3ba74167ab9edda67c41a63e9c5765c69e4
Damir <d.makhmudov@kazan.csoft.ru>
2022-04-28 16:05:29 +0300

    Add project files.

---
+++ KosmosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tdms.Api;
+using Tdms.Log;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Reflection;
+using System.Web;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Timers;
+using System.Net.Http;
+using System.Collections.Generic;
+
+namespace kosmos
+{
+    public class KosmosController : ControllerBase
+    {
+    }
+}
